0b9896833f6793327bdec715d213462681b6ef63
kovalenkou <anekosheik@gmail.com>
2022-07-31 16:37:09 +0300

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import post_list, post_detail
+from .views import post_list, post_detail, post_new, post_edit
 
 
 urlpatterns = [
     path('', post_list.as_view(), name='post_list'),
     path('post/<int:pk>/', post_detail.as_view(), name='post_detail'),
+    path('post/new/', post_new.as_view(), name='post_new'),
+    path('post/<int:pk>/edit/', post_edit.as_view(), name='post_edit'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,31 +1,70 @@
 from pickle import GET
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from django.views import View
 from django.views.generic import TemplateView, CreateView
 
-from blog.models import Post
+from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 class post_list(CreateView):
-    template_name = 'blog/post_list.html'
+    template_name = "blog/post_list.html"
     model = Post
-    fields = ['author', 'title', 'text', 'created_date', 'published_date']
-    
+    fields = ["author", "title", "text", "created_date", "published_date"]
+
     def get(self, request):
-        posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-        return render(request, 'blog/post_list.html', {'posts': posts} )
+        posts = Post.objects.filter(published_date__lte=timezone.now()).order_by(
+            "published_date"
+        )
+        return render(request, "blog/post_list.html", {"posts": posts})
 
-    def post(self, request):
-      return HttpResponse('Class based view')
 
-    
 class post_detail(CreateView):
     # template_name = 'post_detail.html'
     model = Post
-    fields = ['author', 'title', 'text', 'created_date', 'published_date']
+    fields = ["author", "title", "text", "created_date", "published_date"]
+
+    def get(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        return render(request, "blog/post_detail.html", {"post": post})
+
+
+class post_new(CreateView):
+    form_class = Post
+    template_name = 'blog/post_edit.html'
+    
+    def get(self, request):
+        form = PostForm()
+        return render(request, "blog/post_edit.html", {"form": form})
+
+    def post(self, request):
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+
+
+class post_edit(CreateView):
+    form_class = Post
+    template_name = 'blog/post_edit.html'
+
     
     def get(self, request, pk):
         post = get_object_or_404(Post, pk=pk)
-        return render(request, 'blog/post_detail.html', {'post': post})
+        form = PostForm(instance=post)
+        return render(request, "blog/post_edit.html", {"form": form})
+
+    def post(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
