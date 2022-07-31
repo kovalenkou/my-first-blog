aa8c2231818df73c2ff802c2dc085a21614e9261
kovalenkou <anekosheik@gmail.com>
2022-07-31 11:49:27 +0300

    change view post_list

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,23 @@
+from pickle import GET
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
+
+from django.views.generic import TemplateView, CreateView
+
+from blog.models import Post
 
 # Create your views here.
+class post_list(CreateView):
+    template_name = 'blog/post_list.html'
+    model = Post
+    fields = ['author', 'title']
+
+    # def post_list(self, request):
+    #     return render(request, 'blog/post_list.html', {})
+
+    # def get(self, request):
+    #    return HttpResponse('Class based view')
+
+    # def post(self, request):
+    #   return HttpResponse('Class based view')
--- mysite/settings.py
+++ mysite/settings.py
@@ -10,26 +10,27 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 from pickle import STACK_GLOBAL
 
-import local_settings
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = local_settings.SECRET_KEY
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = ['kovalenkou.pythonanywhere.com']
-
+ALLOWED_HOSTS = ['127.0.0.1'] if DEBUG else['kovalenkou.pythonanywhere.com']
 
 # Application definition
 
